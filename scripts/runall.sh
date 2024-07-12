077302c5d25262b5a84a2c329e34623f5a39fa14
Ao Li <leeleo3x@gmail.com>
2024-07-12 12:17:06 -0400

    update.

---
+++ scripts/runall.sh
@@ -0,0 +1,6 @@
+# python3 -m fray_benchmark jacontebe runJPF --name eval-1 --cpu 5
+# python3 -m fray_benchmark jacontebe runRR --name eval-1 --cpu 5
+# python3 -m fray_benchmark jacontebe run --name eval-1 --scheduler random --cpu 5
+# python3 -m fray_benchmark sctbench runJPF --name eval-1 --cpu 5
+# python3 -m fray_benchmark sctbench runRR --name eval-1 --cpu 5
+# python3 -m fray_benchmark sctbench run --name eval-1 --scheduler random --cpu 5
