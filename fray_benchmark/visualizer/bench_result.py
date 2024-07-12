d24f0125d1fb44df03ba9c2e816550567f9c7312
Ao Li <leeleo3x@gmail.com>
2024-07-12 16:32:18 -0400

    udpate.

--- fray_benchmark/visualizer/bench_result.py
+++ fray_benchmark/visualizer/bench_result.py
@@ -28,23 +28,42 @@ class BenchResult:
             if folder == "results":
                 continue
             run_folder = os.path.join(self.path, folder)
-            with open(os.path.join(run_folder, "report.txt")) as f:
-                text = f.read()
-                match = self.error_pattern.search(text)
-                if not match:
-                    continue
-                error_type, value = match.groups()
-                if error_type == "No Error":
-                    summary_file.write(f"{folder},0,{value}\n")
-                elif error_type == "Error Found":
-                    summary_file.write(f"{folder},1,{value}\n")
+            report = open(os.path.join(run_folder, "report.txt"))
+            text = report.read()
+            match = self.error_pattern.search(text)
+            if not match:
+                continue
+            error_type, value = match.groups()
+            if self.tech == "rr":
+                total_iteration = int(text.strip().split("\n")[-2].split(":")[1]) + 1
+            elif self.tech == "jpf":
+                stdout = open(os.path.join(run_folder, "stdout.txt")).readlines()
+                total_iteration = -1
+                for line in reversed(stdout):
+                    if line.startswith("paths ="):
+                        total_iteration = int(line.split("=")[-1].strip())
+                        break
+                if error_type == "Error Found" and total_iteration == -1:
+                    total_iteration = 1
+            else:
+                stdout = open(os.path.join(run_folder, "stdout.txt")).readlines()
+                total_iteration = -1
+                for line in reversed(stdout):
+                    if line.startswith("Starting iteration"):
+                        total_iteration = int(line.split(" ")[-1].strip()) + 1
+                        break
+
+            if error_type == "No Error":
+                summary_file.write(f"{folder},0,{value},{total_iteration}\n")
+            elif error_type == "Error Found":
+                summary_file.write(f"{folder},1,{value},{total_iteration}\n")
 
     def load_csv(self) -> pd.DataFrame:
         result_folder = os.path.join(self.path, "results")
         if not os.path.exists(result_folder):
             raise Exception("No results folder found")
         return pd.read_csv(
-            os.path.join(result_folder, "summary.csv"), names=["run", "error", "time"]
+            os.path.join(result_folder, "summary.csv"), names=["run", "error", "time", "iter"]
         )
 
 
@@ -61,7 +80,7 @@ class BenchmarkSuite:
         for bench in self.benchmarks:
             bench.to_csv()
             df = bench.load_csv()
-            df["tech"] = self.name_remap(bench.tech)
+            df["Technique"] = self.name_remap(bench.tech)
             data.append(df)
         return pd.concat(data)
 
@@ -70,16 +89,19 @@ class BenchmarkSuite:
             return "$\\textsc{Fray}$"
         return name.upper()
 
-    def to_aggregated_fig(self) -> matplotlib.axes.Axes:
+    def to_aggregated_fig(self, measurement: str) -> matplotlib.axes.Axes:
         df = self.to_aggregated_dataframe()
         df = df[df["error"] == 1]
-        df_grouped = df.groupby(['time', 'tech']).size().reset_index(name='count')
+        df_grouped = df.groupby([measurement, 'Technique']).size().reset_index(name='count')
         # Pivot the dataframe to have 'time' as the index and 'tech' as columns
-        df_pivot = df_grouped.pivot(index='time', columns='tech', values='count').fillna(0)
+        df_pivot = df_grouped.pivot(index=measurement, columns='Technique', values='count').fillna(0)
         zero_row = pd.DataFrame(0, index=[0], columns=df_pivot.columns)
         df_pivot = pd.concat([zero_row, df_pivot]).sort_index()
         df_cumsum = df_pivot.cumsum()
         ax = sns.lineplot(data=df_cumsum, linewidth=2, markers=True)
-        ax.set_xlabel('Time (Second)')
-        ax.set_ylabel('Accumulated \# of Bugs')
+        if measurement == "time":
+            ax.set_xlabel('Seconds')
+        else:
+            ax.set_xlabel('Iterations')
+        ax.set_ylabel('Cumulative \# of Bugs')
         return ax
