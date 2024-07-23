389b5d9802483f9789fd05dfc36828b44b707034
Ao Li <leeleo3x@gmail.com>
2024-07-23 18:30:45 -0400

    update.

--- scripts/runexec.sh
+++ scripts/runexec.sh
@@ -1,4 +1,6 @@
-# python3 -m fray_benchmark run fray sctbench --name exec-1 --scheduler random --cpu 32 --perf-mode --timeout 600 --iterations 20
-# python3 -m fray_benchmark run jpf sctbench --name exec-1 --cpu 32 --perf-mode --timeout 600 --iterations 20
-# python3 -m fray_benchmark run rr sctbench --name exec-1 --cpu 32 --perf-mode --timeout 600 --iterations 20
-python3 -m fray_benchmark run fray jacontebe --name exec-1 --scheduler random --cpu 32 --perf-mode --timeout 60 --iterations 1
+python3 -m fray_benchmark run fray sctbench --name exec-1 --scheduler random --cpu 32 --perf-mode --timeout 600 --iterations 1
+python3 -m fray_benchmark run jpf sctbench --name exec-1 --cpu 32 --perf-mode --timeout 600 --iterations 1
+python3 -m fray_benchmark run rr sctbench --name exec-1 --cpu 32 --perf-mode --timeout 600 --iterations 1
+python3 -m fray_benchmark run fray jacontebe --name exec-1 --scheduler random --cpu 32 --perf-mode --timeout 600 --iterations 1
+python3 -m fray_benchmark run jpf jacontebe --name exec-1 --cpu 32 --perf-mode --timeout 600 --iterations 1
+python3 -m fray_benchmark run rr jacontebe --name exec-1 --cpu 32 --perf-mode --timeout 600 --iterations 1
