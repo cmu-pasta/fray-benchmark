e7f33490dcaa5ec6af2a55bbfaa819ecf72e6387
Ao Li <leeleo3x@gmail.com>
2025-01-08 13:40:17 -0500

    update.

--- fray_benchmark/bms/lucenenew.py
+++ fray_benchmark/bms/lucenenew.py
@@ -8,17 +8,17 @@ from ..utils import load_test_cases
 
 class LuceneBenchmark(UnitTestBenchmark):
     def __init__(self) -> None:
-        self.lucene_dir = os.path.join(ARTIFACTS_PATH, "lucene")
+        self.lucene_dir = os.path.join(ARTIFACTS_PATH, "lucene-new")
         super().__init__(
-            "lucene",
+            "lucene-new",
             [
-                os.path.join(self.lucene_dir, "lucene/core/build/libs/*.jar"),
+                os.path.join(self.lucene_dir, "lucene/misc/build/libs/*.jar"),
                 os.path.join(self.lucene_dir,
-                             "lucene/core/build/dependency/*.jar"),
-            ], load_test_cases(os.path.join(ASSETS_PATH, "lucene.txt")),
+                             "lucene/misc/build/dependency/*.jar"),
+            ], load_test_cases(os.path.join(ASSETS_PATH, "lucene-new.txt")),
             {
                 "tests.seed": "deadbeef",
-                "tests.jvmForkArgsFile": os.path.join(self.lucene_dir, "lucene/core/build/tmp/test/jvm-forking.properties"),
+                "tests.jvmForkArgsFile": os.path.join(self.lucene_dir, "lucene/misc/build/tmp/test/jvm-forking.properties"),
             },
             True)
 
