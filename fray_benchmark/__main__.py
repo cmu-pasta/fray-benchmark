54fe987fc5be8026760c1fa7385365d1596a2df6
Ao Li <leeleo3x@gmail.com>
2025-03-06 14:27:00 -0500

    update.

--- fray_benchmark/__main__.py
+++ fray_benchmark/__main__.py
@@ -111,7 +111,7 @@ def run_single(path: str, debug_jvm: bool, no_fray: bool):
         shutil.rmtree(out_dir)
     os.makedirs(out_dir)
     fray_args = [
-        "--scheduler=pos",
+        "--scheduler=random",
     ]
     if no_fray:
         fray_args.append("--no-fray")
