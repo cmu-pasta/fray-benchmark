a0b2fd7f6c1ee35b51cf49a1b29c6c8971c71665
Ao Li <aoli.al@hotmail.com>
2025-03-29 09:04:30 -0400

    Add curator framework.

--- fray_benchmark/bm_configs/curator.py
+++ fray_benchmark/bm_configs/curator.py
@@ -29,3 +29,27 @@ class CuratorRecipesBenchmark(UnitTestBenchmark):
             "./mvnw",
             "dependency:copy-dependencies",
         ], cwd=self.target_dir)
+
+class CuratorFrameworkBenchmark(UnitTestBenchmark):
+    def __init__(self) -> None:
+        self.target_dir = os.path.join(ARTIFACTS_PATH, "curator")
+        super().__init__(
+            "curator-framework",
+            [
+                os.path.join(self.target_dir, "curator-framework/target/curator-framework-5.8.1-SNAPSHOT.jar"),
+                os.path.join(self.target_dir, "curator-framework/target/curator-framework-5.8.1-SNAPSHOT-tests.jar"),
+                os.path.join(self.target_dir, "curator-framework/target/dependency/*.jar"),
+            ], load_test_cases(os.path.join(ASSETS_PATH, "curator-framework.txt")),
+            {},
+            False)
+
+    def build(self) -> None:
+        subprocess.call([
+            "./mvnw",
+            "install",
+            "-DskipTests",
+        ], cwd=self.target_dir)
+        subprocess.call([
+            "./mvnw",
+            "dependency:copy-dependencies",
+        ], cwd=self.target_dir)
